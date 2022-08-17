d09c74f610e3c433fb1e690bb887b536a790cd99
Itamar Shechter <84578656+itamar-sh@users.noreply.github.com>
2022-08-17 17:38:09 +0300

    Add files via upload

---
+++ Ex1 - Run time of system calls/osm.h
@@ -0,0 +1,36 @@
+//
+// Created by itama on 13/03/2022.
+//
+
+#ifndef _OSM_H
+#define _OSM_H
+
+
+/* calling a system call that does nothing */
+#define OSM_NULLSYSCALL asm volatile( "int $0x80 " : : \
+        "a" (0xffffffff) /* no such syscall */, "b" (0), "c" (0), "d" (0) /*:\
+        "eax", "ebx", "ecx", "edx"*/)
+
+
+/* Time measurement function for a simple arithmetic operation.
+   returns time in nano-seconds upon success,
+   and -1 upon failure.
+   */
+double osm_operation_time(unsigned int iterations);
+
+
+/* Time measurement function for an empty function call.
+   returns time in nano-seconds upon success,
+   and -1 upon failure.
+   */
+double osm_function_time(unsigned int iterations);
+
+
+/* Time measurement function for an empty trap into the operating system.
+   returns time in nano-seconds upon success,
+   and -1 upon failure.
+   */
+double osm_syscall_time(unsigned int iterations);
+
+
+#endif
